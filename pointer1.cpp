9184f82365b8433f2e5b04430423d7623251cdc1
Yves Asidillo <yvesasidillo@gmail.com>
2016-12-07 16:50:36 +0800

    Create pointer1.cpp

---
+++ pointer1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	
+	
+	int a;
+	int *pt;
+	
+	pt = &a;
+	
+	cout<<"Enter number: ";
+	cin>>a;
+	cout<<endl;
+	
+	cout<<"The pointer values is "<<*pt<<" the memory value is: "<<pt;
+	
+	
+	
+		
+}
