6792076a118caaf9a8c67b55ae47f52ead411135
Yves Asidillo <yvesasidillo@gmail.com>
2017-01-19 08:14:05 +0800

    added push

---
+++ linkedlist_stack.cpp
@@ -0,0 +1,82 @@
+#include<stdio.h> 
+#include<stdlib.h>
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+struct node {
+	int data;
+	struct node * next;
+} *head;
+
+void display(struct node * r) {
+  r = head;
+  if (r == NULL) {
+    return;
+  }
+  while (r != NULL) {
+    printf("%d ", r -> data);
+    r = r -> next;
+  }
+  printf("\n");
+}
+
+void push(int num) {
+    int push;
+    struct node *ptr;
+    cout<<"Enter a number to insert:";
+    cin>>push;
+    ptr=new node;
+    ptr->data=push;
+    ptr->next=NULL;
+    if(head!=NULL)
+        ptr->next=head;
+    head=ptr;
+    cout<<"Push successful";
+}
+
+int main() {
+  int i, num, loc;
+  struct node * n;
+  head = NULL;
+  while (1) {
+  	system("cls");
+    cout<<"Linked List Operations\n";
+    cout<<"===============\n";
+    cout<<"1.Push\n";
+    cout<<"2.Pop\n";
+    cout<<"3.Display\n";
+    cout<<"4.Exit\n";
+    cout<<"Enter your choice : ";
+    cin>>i;
+    if (i <= 0) {
+      cout<<"Enter only an Integer\n";
+      exit(0);
+    } else {
+      switch (i) {
+      	  	  
+	  case 1:
+  		push(num);
+  		break;
+  		
+
+  	  	
+  	  case 3:
+        if (head == NULL) {
+        cout<<"List is Empty\n";
+        } else {
+        cout<<"Element(s) in the list are : ";
+          }
+        display(n);
+        break;
+  	  	
+  	  case 4:
+       return 0; 
+       default:
+        cout<<"Invalid option\n";
+       }
+    }
+   	getch();    
+  }
+   return 0;
+}
